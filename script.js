31e3174ec82ece5057a0e9aca8f2f969c28384d9
mrgnschndr <mrgnschndr@gmail.com>
2024-09-14 12:10:32 -0600

    Add alert for an incorrect guess and if number was high or low.

--- script.js
+++ script.js
@@ -3,28 +3,45 @@ let userName = window.prompt("Hi!\nWhat's your name?");
 // Determine random number between 1 and 50
 let randomNumber = Math.floor(Math.random() * 51) + 1;
 console.log(randomNumber);
+let usedGuesses = 0;
 // Prompt user to guess a random number (1)
 let userGuess1 = window.prompt(`${userName}, please guess a number between 1 and 50.`);
+usedGuesses ++;
+let remainingGuesses = 3 - usedGuesses;
 
 // NOTE: If a user's guess is incorrect, respond with a prompt letting them know whether or not their guess was too high or too low and how many chances remain.
 if (userGuess1 == randomNumber) {
     window.alert(`CONGRATULATIONS! You've won the game by guessing the random number ${randomNumber}!`);
+} else if (userGuess1 > randomNumber) {
+    window.alert(`Incorrect! Your guess was too high. You have ${remainingGuesses} guesses remaining.`);
+} else if (userGuess1 < randomNumber) {
+    window.alert(`Incorrect! Your guess was too low. You have ${remainingGuesses} guesses remaining.`);
 }
 
 
 
 // Prompt user to guess a random number (2)
 let userGuess2 = window.prompt(`${userName}, please guess another random number between 1 and 50.`);
+usedGuesses ++;
 
 if (userGuess2 == randomNumber) {
     window.alert(`CONGRATULATIONS! You've won the game by guessing the random number ${randomNumber}!`);
+} else if (userGuess2 > randomNumber) {
+    window.alert(`Incorrect! Your guess was too high. You have ${remainingGuesses} guesses remaining.`);
+} else if (userGuess2 < randomNumber) {
+    window.alert(`Incorrect! Your guess was too low. You have ${remainingGuesses} guesses remaining.`);
 }
 
 
 
 // Prompt user to guess a random number (3)
 let userGuess3 = window.prompt(`${userName}, please guess one more random number between 1 and 50.`);
+usedGuesses ++;
 
 if (userGuess3 == randomNumber) {
     window.alert(`CONGRATULATIONS! You've won the game by guessing the random number ${randomNumber}!`);
+} else if (userGuess3 > randomNumber) {
+    window.alert(`Incorrect! Your guess was too high. You have ${remainingGuesses} guesses remaining.`);
+} else if (userGuess3 < randomNumber) {
+    window.alert(`Incorrect! Your guess was too low. You have ${remainingGuesses} guesses remaining.`);
 }
