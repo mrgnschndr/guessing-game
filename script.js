8aeb4878907a7b7a10ed4c1c758b0784e5cfd8ac
mrgnschndr <mrgnschndr@gmail.com>
2024-09-14 11:44:55 -0600

    Add all prompts for user guessing a random number.

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ let userName = window.prompt("Hi!\nWhat's your name?");
 
 // Determine random number between 1 and 50
 let randomNumber = Math.floor(Math.random() * 51) + 1;
-// Prompt user to guess a random number
+// Prompt user to guess a random number (1)
 let userGuess1 = window.prompt(`${userName}, please guess a number between 1 and 50.`);
+
+
+
+// Prompt user to guess a random number (2)
+let userGuess2 = window.prompt(`${userName}, please guess another random number between 1 and 50.`);
+
+
+
+// Prompt user to guess a random number (3)
+let userGuess3 = window.prompt(`${userName}, please guess one more random number between 1 and 50.`);
