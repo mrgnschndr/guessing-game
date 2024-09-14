90f60c6c5c9c39516a6634d2b9efbad85c123a06
mrgnschndr <mrgnschndr@gmail.com>
2024-09-14 11:38:14 -0600

    Define variable & generate random number between 1 and 50. Define variable & let user guess a random number and use a template literal with their name.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 let userName = window.prompt("Hi!\nWhat's your name?");
+
+// Determine random number between 1 and 50
+let randomNumber = Math.floor(Math.random() * 51) + 1;
+// Prompt user to guess a random number
+let userGuess1 = window.prompt(`${userName}, please guess a number between 1 and 50.`);
